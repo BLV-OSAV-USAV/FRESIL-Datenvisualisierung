e6c5fd9b46604f33a12f525e51923909d41df661
Maelle <maelle.wannier@protonmail.ch>
2023-12-04 17:00:31 +0100

    enhance datatable

--- python-test/app.py
+++ python-test/app.py
@@ -1,18 +1,14 @@
 import dash
-from dash import Dash, html, dcc, dash_table
+from dash import html, dcc, dash_table
 from dash.dependencies import Input, Output, State
-import pandas as pd
-import plotly.express as px
 import dash_bootstrap_components as dbc
-from bs4 import BeautifulSoup
 import data_prep
 from dash.exceptions import PreventUpdate
-from datetime import datetime
-
+import pandas as pd
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-
+# Load data using data_prep functions
 gefahr = data_prep.html_parser('../csv-files/ad_gefahr-20231128.csv')
 meldung = data_prep.public_data(data_prep.html_parser('../csv-files/ad_meldung-20231128.csv'))
 steckbrief = data_prep.public_data(data_prep.html_parser('../csv-files/ad_steckbrief-20231128.csv'))
@@ -21,11 +17,9 @@ steckbriefXmeldung = data_prep.html_parser('../csv-files/ad_meldung_ad_steckbrie
 steckbriefXgefahr = data_prep.html_parser('../csv-files/ad_steckbrief_ad_gefahr-20231128.csv')
 meldungXgefahr = data_prep.html_parser('../csv-files/ad_meldung_ad_gefahr-20231128.csv')
 
-
 gefahr_counts = meldungXgefahr['gefahr_id'].value_counts().reset_index()
 gefahr_counts.columns = ['id', 'count']
 gefahr_counts = pd.merge(gefahr_counts, gefahr[['id', 'bezeichnung_de']], on='id', how='left')
-
 
 # Initialize the list directly in the layout
 bezeichnung_list = [html.Li(bez) for bez in gefahr_counts['bezeichnung_de']]
@@ -45,18 +39,17 @@ collapse = html.Div(
     ]
 )
 
-meldung_table = dash_table.DataTable(meldung.to_dict('records'), [{"name": i, "id": i} for i in meldung.columns])
+meldung_table = dash_table.DataTable(data=data_prep.load_meldung_data(), columns=data_prep.meldung_columns(meldung))
 
 date = html.Center(dcc.DatePickerRange(start_date_placeholder_text="Start Period",
                             end_date_placeholder_text="End Period", id='date_filter'))
 
-
 app.layout = html.Div([
     html.H1('ADURA'),
     html.Div([
-        dcc.Graph(id= 'treemap', figure=data_prep.create_treemap(gefahr_counts, 20)),
+        dcc.Graph(id='treemap', figure=data_prep.create_treemap(gefahr_counts, 20)),
         date
-        ]),
+    ]),
     html.Br(),
     html.Div([
         collapse
@@ -65,7 +58,6 @@ app.layout = html.Div([
         meldung_table
     ], id='tbl')
 ])
-
 
 
 @app.callback(
@@ -108,7 +100,13 @@ def display_selected_data(click_data):
 
         selected_meldung_table = dash_table.DataTable(style_data={'whiteSpace': 'normal', 'height': 'auto'}, 
                                                                     data = selected_meldung_table.to_dict('records'), 
-                                                                    columns = [{"name": i, "id": i} for i in selected_meldung_table.columns], 
+                                                                    columns = [{"name": i, "id": i} for i in selected_meldung_table.columns],
+                                                                    filter_action="native",
+                                                                    sort_action="native",
+                                                                    sort_mode="multi",
+                                                                    page_action="native",
+                                                                    page_current= 0,
+                                                                    page_size= 5,
                                                                     style_cell={'textAlign': 'left'})
         
         return [selected_meldung_table]
--- python-test/data_prep.py
+++ python-test/data_prep.py
@@ -2,19 +2,25 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import plotly.express as px
 
-
 def html_parser(path):
     csv_file = pd.read_csv(path, sep='|')
     csv_file = csv_file.applymap(lambda x: BeautifulSoup(x, 'html.parser').text if isinstance(x, str) else x)
     return csv_file
 
 def public_data(doc):
-    doc = doc[doc['is_public'] == True] 
-    return doc
-
+    return doc[doc['is_public']]
 
 def create_treemap(gefahr_counts, initial_categories):
     return px.treemap(gefahr_counts.head(initial_categories),
                      path=['bezeichnung_de'],
                      values='count',
                      title=f'Top {initial_categories} Gefahr')
+                     
+def load_meldung_data():
+    # Load and preprocess meldung data
+    meldung = public_data(html_parser('../csv-files/ad_meldung-20231128.csv'))
+    return meldung.to_dict('records')
+
+def meldung_columns(meldung):
+    # Define columns for meldung_table
+    return [{"name": i, "id": i} for i in meldung.columns]

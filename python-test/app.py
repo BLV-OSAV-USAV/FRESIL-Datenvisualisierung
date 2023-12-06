44ae66c315491100e181fc7b3f751a8318ea4d7d
Maelle <maelle.wannier@protonmail.ch>
2023-12-06 15:02:16 +0100

    update python app

--- python-test/app.py
+++ python-test/app.py
@@ -27,7 +27,7 @@ bezeichnung_list = [html.Li(bez) for bez in gefahr_counts['bezeichnung_de']]
 
 
 date = html.Center(dcc.DatePickerRange(start_date_placeholder_text="Start Period",
-                            end_date_placeholder_text="End Period", id='date_filter'))
+                            end_date_placeholder_text="End Period", id='date_filter', display_format='DD/MM/YYYY', updatemode = 'bothdates', clearable=True))
 
 app.layout = html.Div([
     html.H1('ADURA'),
@@ -57,28 +57,30 @@ def update_treemap(start_date, end_date, top_gefahr_value):
 
     trigger_id = ctx.triggered_id.split(".")[0]
 
+    # Your existing code to filter based on date and create the treemap
+    meldung['erf_date'] = pd.to_datetime(meldung['erf_date'], errors='coerce')
+    
+    if start_date and end_date:
+        meldung_datum = meldung[(meldung['erf_date'] >= start_date) & (meldung['erf_date'] <= end_date)]
+        gefahr_datum = meldungXgefahr[meldungXgefahr['meldung_id'].isin(meldung_datum['id'])]
+    else:
+        # Use the entire dataset if start_date or end_date is missing
+        gefahr_datum = meldungXgefahr
+
+    gefahr_counts_filtered = gefahr_datum['gefahr_id'].value_counts().reset_index()
+    gefahr_counts_filtered.columns = ['id', 'count']
+    gefahr_counts_filtered = pd.merge(gefahr_counts_filtered, gefahr[['id', 'bezeichnung_de']], on='id', how='left')
+
     if trigger_id == "date_filter":
         if not start_date or not end_date:
             raise PreventUpdate
 
-        # Your existing code to filter based on date and create the treemap
-        meldung['erf_date'] = pd.to_datetime(meldung['erf_date'], errors='coerce')
-        meldung_datum = meldung[(meldung['erf_date'] >= start_date) & (meldung['erf_date'] <= end_date)]
-        gefahr_datum = meldungXgefahr[meldungXgefahr['meldung_id'].isin(meldung_datum['id'])]
-        gefahr_counts_filtered = gefahr_datum['gefahr_id'].value_counts().reset_index()
-        gefahr_counts_filtered.columns = ['id', 'count']
-        gefahr_counts_filtered = pd.merge(gefahr_counts_filtered, gefahr[['id', 'bezeichnung_de']], on='id', how='left')
-        return data_prep.create_treemap(gefahr_counts_filtered, 20)
-
     elif trigger_id == "top_gefahr":
         if top_gefahr_value is None:
             raise PreventUpdate
-        else:
-            # Your existing code to create the treemap based on top_gefahr_value
-            return data_prep.create_treemap(gefahr_counts, top_gefahr_value)
 
-    else:
-        raise PreventUpdate
+    # Your existing code to create the treemap based on gefahr_counts_filtered
+    return data_prep.create_treemap(gefahr_counts_filtered, top_gefahr_value)
 
 
 

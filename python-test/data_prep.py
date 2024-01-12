df173611d2df1414bd20232782744cb7e5e41ec0
Maelle <maelle.wannier@protonmail.ch>
2024-01-12 14:36:31 +0100

    add treiber infos

--- python-test/data_prep.py
+++ python-test/data_prep.py
@@ -5,7 +5,7 @@ from dash import html, dcc, dash_table
 
 def html_parser(path):
     csv_file = pd.read_csv(path, sep='|')
-    csv_file = csv_file.applymap(lambda x: BeautifulSoup(x, 'html.parser').text if isinstance(x, str) else x)
+    csv_file = csv_file.map(lambda x: BeautifulSoup(x, 'html.parser').text if isinstance(x, str) else x)
     return csv_file
 
 def public_data(doc):

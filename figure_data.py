6a0835945a983036152eeeddcc945eead9affdf9
Maelle <maelle.wannier@protonmail.ch>
2024-01-18 17:14:45 +0100

    update

--- figure_data.py
+++ figure_data.py
@@ -9,9 +9,8 @@ def count_gefahr():
 
     gefahr_counts = meldungXgefahr['gefahr_id'].value_counts().reset_index()
     gefahr_counts.columns = ['id', 'count']
-    gefahr_counts = pd.merge(gefahr_counts, gefahr[['id', 'bezeichnung_de', 'erf_date']], on='id', how='left')
-    gefahr_counts['Dates'] = pd.to_datetime(gefahr_counts['erf_date']).dt.date
-    gefahr_counts['Time'] = pd.to_datetime(gefahr_counts['erf_date']).dt.time
+    gefahr_counts = pd.merge(gefahr_counts, gefahr[['id', 'bezeichnung_de']], on='id', how='left')
+
 
     gefahr_counts.to_csv('./figure_data/gefahr_counts.csv', sep='#', quotechar='`', index=False)
 
@@ -43,6 +42,6 @@ def count_meldung_pro_tag():
 
 
 
-#count_gefahr()
+count_gefahr()
 #count_gefahr_pro_tag()
-count_meldung_pro_tag()
+#count_meldung_pro_tag()

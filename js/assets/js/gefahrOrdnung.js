e7d6c9d05863446e8b8083a63a0024334d107ea4
Maelle <maelle.wannier@protonmail.ch>
2024-02-28 11:26:16 +0100

    add bereich to array

--- js/assets/js/gefahrOrdnung.js
+++ js/assets/js/gefahrOrdnung.js
@@ -1,40 +1,27 @@
-// Load data from CSV files: gefahrCounts and treiberCounts
-Promise.all([
-    d3.csv("../figure_data/gefahr_counts.csv"),
-    d3.csv("../figure_data/gefahr_treiber_counts.csv")
-  ]).then(([gefahrCounts, treiberCounts]) => {
-    // Merge the data on 'id' and 'gefahr_id'
-    const mergedData = gefahrCounts.map(count => ({
-      ...count,
-      ...treiberCounts.find(treiber => treiber.gefahr_id === count.id)
-    }));
-
-    // Convert the merged data to the desired structure
-    let result = mergedData.map(entry => {
-      const {
-        id,
-        count,
-        bezeichnung_de,
-        mean_sterne,
-        gefahr_id,
-        ...treiberColumns
-      } = entry;
-      const treiber = Object.keys(treiberColumns).reduce((acc, key) => {
-        acc[key] = treiberColumns[key];
-        return acc;
-      }, {});
-      const size = count * mean_sterne;
-      return {
-        id: +id,
-        name: bezeichnung_de,
-        count: +count,
-        mean_sterne: +mean_sterne,
-        treiber: treiber,
-        size: +size
-      };
-    });
+    // Load data from CSV files: gefahrCounts and treiberCounts
+    Promise.all([
+      d3.csv("../figure_data/gefahr_counts.csv"),
+      d3.csv("../figure_data/gefahr_treiber_counts.csv"),
+      d3.csv("../figure_data/gefahr_bereich_counts.csv")
+    ]).then(([gefahrCounts, treiberCounts, bereichCounts]) => {
+      // Merge the data on 'id' and 'gefahr_id'
+      const mergedData = gefahrCounts.map(count => {
+        const treiberData = treiberCounts.find(treiber => treiber.gefahr_id === count.id);
+        const bereichData = bereichCounts.find(bereich => bereich.gefahr_id === count.id);
+        const size = count.count * count.mean_sterne;
+        return {
+          id: +count.id,
+          name: count.bezeichnung_de,
+          count: +count.count,
+          mean_sterne: +count.mean_sterne,
+          size: +size,
+          treiber: treiberData ? Object.fromEntries(Object.entries(treiberData).slice(1)) : {}, // Convert treiberData object to key-value pairs
+          bereich: bereichData ? Object.fromEntries(Object.entries(bereichData).slice(1)) : {}  // Convert bereichData object to key-value pairs
+        };
+      });
+    
 
     // Call the baseVisualization function with the result data
-    baseVisualization(result, "#cab2d6", 'gefahr');
+    baseVisualization(mergedData, "#cab2d6", 'gefahr');
 
   })
--- js/assets/js/matrixOrdnung.js
+++ js/assets/js/matrixOrdnung.js
@@ -1,42 +1,26 @@
 // Load the required CSV files asynchronously
 Promise.all([
   d3.csv("../figure_data/matrix_counts.csv"), // Load matrix counts data
-  d3.csv("../figure_data/matrix_treiber_counts.csv") // Load matrix treiber counts data
-]).then(([matrixCounts, treiberCounts]) => {
+  d3.csv("../figure_data/matrix_treiber_counts.csv"), // Load matrix treiber counts data
+  d3.csv("../figure_data/matrix_bereich_counts.csv")
+]).then(([matrixCounts, treiberCounts, bereichCounts]) => {
   // Merge the data on 'id' and 'matrix_id'
-  const mergedData = matrixCounts.map(count => ({
-    ...count,
-    ...treiberCounts.find(treiber => treiber.matrix_id === count.id)
-  }));
-
-  // Store the initial viewBox value
-  const initialViewBox = "-250 -250 500 500";
-
-  // Convert the merged data to the desired structure
-  let result = mergedData.map(entry => {
-    const {
-      id,
-      count,
-      bezeichnung_de,
-      mean_sterne,
-      matrix_id,
-      ...treiberColumns
-    } = entry;
-    const treiber = Object.keys(treiberColumns).reduce((acc, key) => {
-      acc[key] = treiberColumns[key];
-      return acc;
-    }, {});
-    const size = count * mean_sterne;
+  const mergedData = matrixCounts.map(count => {
+    const treiberData = treiberCounts.find(treiber => treiber.matrix_id === count.id);
+    const bereichData = bereichCounts.find(bereich => bereich.matrix_id === count.id);
+    const size = count.count * count.mean_sterne;
     return {
-      id: +id,
-      name: bezeichnung_de,
-      count: +count,
-      mean_sterne: +mean_sterne,
-      treiber: treiber,
-      size: +size
+      id: +count.id,
+      name: count.bezeichnung_de,
+      count: +count.count,
+      mean_sterne: +count.mean_sterne,
+      size: +size,
+      treiber: treiberData ? Object.fromEntries(Object.entries(treiberData).slice(1)) : {}, // Convert treiberData object to key-value pairs
+      bereich: bereichData ? Object.fromEntries(Object.entries(bereichData).slice(1)) : {}  // Convert bereichData object to key-value pairs
     };
   });
 
-  // Call the baseVisualization function with the result data
-  baseVisualization(result, '#a6cee3', 'matrix');
+
+  // Call the baseVisualization function with the result datas
+  baseVisualization(mergedData, '#a6cee3', 'matrix');
 });

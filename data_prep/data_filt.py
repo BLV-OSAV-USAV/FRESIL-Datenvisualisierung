e7a116f55d87e27859d8a65cda3b2559284e58b2
Maelle <maelle.wannier@protonmail.ch>
2024-05-01 07:35:26 +0200

    code cleaning

--- data_prep/data_filt.py
+++ data_prep/data_filt.py
@@ -4,104 +4,202 @@ import os
 import re
 
 def html_parser(path):
-    # Read CSV file using pandas with '|' as the separator
+    """
+    Parse HTML content in CSV file cells using BeautifulSoup.
+
+    Parameters:
+    - path (str): Path to the CSV file.
+
+    Returns:
+    - DataFrame: Parsed DataFrame with HTML content replaced by text.
+    """
     csv_file = pd.read_csv(path, sep='|')
-    # Apply BeautifulSoup to each cell in the DataFrame to extract text from HTML
     csv_file = csv_file.applymap(lambda x: BeautifulSoup(x, 'html.parser').text if isinstance(x, str) else x)
     return csv_file
 
 def format_single_line(entry):
-    # get rid of line breaks
+    """
+    Format text to remove line breaks.
+
+    Parameters:
+    - entry (str): Input text.
+
+    Returns:
+    - str: Text with line breaks removed.
+    """
     if isinstance(entry, str):
         return re.sub(r'\s+', ' ', entry)
     else:
         return entry
 
 def remove_stx_characters(entry):
-    # Replace ASCII STX character (code 2) with an empty string
+    """
+    Remove ASCII STX characters.
+
+    Parameters:
+    - entry (str): Input text.
+
+    Returns:
+    - str: Text with STX characters removed.
+    """
     if isinstance(entry, str):
         return entry.replace('\x02', '')
     else:
         return entry
 
 def public_data(file, column):
-    # Filter rows where 'is_public' column has True values
+    """
+    Filter rows with True values in a specific column.
+
+    Parameters:
+    - file (DataFrame): Input DataFrame.
+    - column (str): Column name.
+
+    Returns:
+    - DataFrame: Filtered DataFrame.
+    """
     return file[file[column]]
 
 def autorisierung_dok(file, column):
-    # Filter rows where 'autorisierung' column has a value of 10 (Public)
+    """
+    Filter rows with a specific value in a column.
+
+    Parameters:
+    - file (DataFrame): Input DataFrame.
+    - column (str): Column name.
+
+    Returns:
+    - DataFrame: Filtered DataFrame.
+    """
     return file[file[column] == 10]
 
 def open_status(file, column):
-    # Filter rows where 'status' column has values 20 (Freigegeben) or 70 (Abgeschlossen)
+    """
+    Filter rows with specific values in a column.
+
+    Parameters:
+    - file (DataFrame): Input DataFrame.
+    - column (str): Column name.
+
+    Returns:
+    - DataFrame: Filtered DataFrame.
+    """
     return file[file[column].isin([20, 70])]
 
 def convert_to_bool(file, column):
-    #convert boolean value to be compatible to cube creator
-    file['signal'] = file['signal'].map({True: 'true', False: 'false'})
+    """
+    Convert boolean values to string representation.
+
+    Parameters:
+    - file (DataFrame): Input DataFrame.
+    - column (str): Column name.
+
+    Returns:
+    - DataFrame: Updated DataFrame.
+    """
+    file[column] = file[column].map({True: 'true', False: 'false'})
     return file
 
 def convert_to_int(file, column):
-    #convert to int datatype
+    """
+    Convert column to integer datatype.
+
+    Parameters:
+    - file (DataFrame): Input DataFrame.
+    - column (str): Column name.
+
+    Returns:
+    - DataFrame: Updated DataFrame.
+    """
     file[column] = file[column].round().astype('Int64') 
     return file   
 
 def convert_datetime_format(file, column):
-    # change the format of dates to be compatible with cube creator
+    """
+    Convert date format to be compatible with cube creator.
+
+    Parameters:
+    - file (DataFrame): Input DataFrame.
+    - column (str): Column name.
+
+    Returns:
+    - DataFrame: Updated DataFrame.
+    """
     for column in ['erf_date','mut_date']:
         file[column] = pd.to_datetime(file[column], format='%d.%m.%Y %H:%M:%S').dt.strftime('%Y-%m-%dT%H:%M:%S')
         file[f'Dates_{column}'] = pd.to_datetime(file[column]).dt.date
     return file
 
 def change_Unspecified_matrix(file,column):
+    """
+    Change values in a specific column.
+
+    Parameters:
+    - file (DataFrame): Input DataFrame.
+    - column (str): Column name.
+
+    Returns:
+    - DataFrame: Updated DataFrame.
+    """
     dc = {
         'Nicht spezifiziert': 'Diverse',
-
         'Non spécifié': 'Divers',
-
         'Non specificato': 'Alimenti vari',
-
         'Unclassified': 'Diverse foods'
     }
-    file.replace(dc, inplace = True)
-
+    file.replace(dc, inplace=True)
     return file
 
 def filter_unspecified_matrixXmeldung(file):
+    """
+    Filter rows based on specific conditions.
+
+    Parameters:
+    - file (DataFrame): Input DataFrame.
+
+    Returns:
+    - DataFrame: Updated DataFrame.
+    """
     meldung_ids = file[file['matrix_id'] == 1].meldung_id
-
-    meldung_id_2 = file[(file['meldung_id'].isin(meldung_ids)) & (file['matrix_id'] != 1)].meldung_id
-
-    # Create a boolean mask to identify rows to drop
+    meldung_id_2 = file[(file['meldung_id'].isin(meldung_ids)) & (file['matrix_id'] != 1)]
     rows_to_drop = (file['meldung_id'].isin(meldung_id_2)) & (file['matrix_id'] == 1)
-
-    # Drop the rows using the boolean mask
     file = file[~rows_to_drop]
-
     return file
 
 def filter_nonOpen_meldung(file):
+    """
+    Filter rows based on data from another file.
+
+    Parameters:
+    - file (DataFrame): Input DataFrame.
+
+    Returns:
+    - DataFrame: Updated DataFrame.
+    """
     meldung = pd.read_csv('./csv-files/ad_meldung-20231128.csv', sep='|')
     meldung_ids = meldung['id'] 
-
     file = file[file['meldung_id'].isin(meldung_ids)]
     return file
 
-
 def prep_data(input_path, output_path):
+    """
+    Prepare data by applying various transformations.
+
+    Parameters:
+    - input_path (str): Path to the input CSV file.
+    - output_path (str): Path to save the output CSV file.
+
+    Returns:
+    - DataFrame: Processed DataFrame.
+    """
     try:
         file = html_parser(input_path)
-
-        # Apply remove_stx_characters to each cell in the DataFrame
-        file = file.applymap(remove_stx_characters)
-
-        # Apply format_single_line to each cell in the DataFrame
-        file = file.applymap(format_single_line)
+        file = file.apply(remove_stx_characters, axis=1)
+        file = file.apply(format_single_line, axis=1)
 
         if input_path == './csv-files/ad_meldung_ad_matrix-20231128.csv':
             file = filter_unspecified_matrixXmeldung(file)
 
-        # Define functions to filter data based on specific columns
         column_functions = {
             'is_public': public_data,
             'autorisierung': autorisierung_dok,
@@ -114,19 +212,16 @@ def prep_data(input_path, output_path):
             #'bezeichnung_de': change_Unspecified_matrix
         }
 
-        # Apply the specified filter function for each column
         for column, func in column_functions.items():
             if column in file.columns:
                 file = func(file, column)
 
-        # Save the filtered DataFrame to a new CSV file using '#' as the separator, " as quotation, and utf-8 encoding
         if input_path == './csv-files/ad_publikation_detail-20231128.csv':
             return file.to_csv(output_path, sep='|', quotechar='"', index=False, encoding='utf-8-sig')
         else:
             return file.to_csv(output_path, sep='#', quotechar='`', index=False, encoding='utf-8-sig')
 
     except pd.errors.EmptyDataError:
-        # Handle the case where the file is empty
         print(f"Warning: {input_path} is empty. Skipping.")
 
 # Define input and output directories
@@ -142,5 +237,3 @@ for file in os.listdir(input_directory):
 
         # Run the prep_data function for each CSV file
         df = prep_data(input_path, output_path)
-
-
--- data_prep/figure_data.py
+++ data_prep/figure_data.py
@@ -5,7 +5,18 @@ import pandas as pd
 import numpy as np
 
 def count_gefahr(timeFilter, bereichName, lg):
-    # Charger les fichiers CSV
+    """
+    Count occurrences of 'gefahr' and related data.
+
+    Parameters:
+    - timeFilter (str): Time filter ('all', 'week', 'month', 'year').
+    - bereichName (str): Bereich name filter ('all' or specific name).
+    - lg (str): Language for treiber bezeichnung ('de', 'fr', 'it', 'en').
+
+    Returns:
+    - CSV files with counts and related data.
+    """
+    # Load CSV files
     meldungXgefahr = pd.read_csv('./csv-files-filtered/filtered-ad_meldung_ad_gefahr-20231128.csv', sep='#', quotechar='`')
     gefahr = pd.read_csv('./csv-files-filtered/filtered-ad_gefahr-20231128.csv', sep='#', quotechar='`')
     meldung = pd.read_csv('./csv-files-filtered/filtered-ad_meldung-20231128.csv', sep='#', quotechar='`')
@@ -14,17 +25,18 @@ def count_gefahr(timeFilter, bereichName, lg):
     treiber = pd.read_csv('./csv-files-filtered/filtered-ad_treiber-20231128.csv', sep='#', quotechar='`')
     treiberXmeldung = pd.read_csv('./csv-files-filtered/filtered-ad_meldung_ad_treiber-20231128.csv', sep='#', quotechar='`')
 
+    # Apply time filter
     if timeFilter != 'all':
         meldung['erfDate'] = pd.to_datetime(meldung['erf_date']).dt.date
-        today = pd.to_datetime('2023-11-28') #pd.to_datetime('today').date()
-
+        today = pd.to_datetime('2023-11-28')
+        
         if timeFilter == 'week':
             start = (today - pd.DateOffset(weeks=1)).date()
         elif timeFilter == 'month':
             start = (today - pd.DateOffset(months=1)).date()
         elif timeFilter == 'year':
             start = (today - pd.DateOffset(years=1)).date()
-
+        
         meldung = meldung[meldung['erfDate'] >= start]
         meldung_time_ids = meldung['id']
         meldungXgefahr = meldungXgefahr[meldungXgefahr['meldung_id'].isin(meldung_time_ids)]
@@ -32,6 +44,7 @@ def count_gefahr(timeFilter, bereichName, lg):
         meldung_ids = meldung['id']
         meldungXgefahr = meldungXgefahr[meldungXgefahr['meldung_id'].isin(meldung_ids)]
 
+    # Apply Bereich filter
     if bereichName != 'all':
         selected_bereich_id = bereich.loc[bereich['bezeichnung_de'] == bereichName, 'id'].values[0]
         meldung_bereich_ids = bereichXmeldung.loc[bereichXmeldung['bereich_id'] == selected_bereich_id, 'meldung_id'].values
@@ -41,36 +54,29 @@ def count_gefahr(timeFilter, bereichName, lg):
     meldung_ids_list = meldungXgefahr.groupby('gefahr_id')['meldung_id'].apply(list).reset_index()
     meldung_ids_list.columns = ['id', 'meldung_ids']
 
-
-    # Compter le nombre de meldung par gefahr
+    # Count occurrences of gefahr_id
     gefahr_counts = meldungXgefahr['gefahr_id'].value_counts().reset_index()
     gefahr_counts.columns = ['id', 'count']
 
-    
-    # Calculer la moyenne des valeurs de 'sterne' par gefahr
+    # Calculate mean 'sterne' values per gefahr_id
     merged_df = pd.merge(meldungXgefahr, meldung[['id', 'sterne']], left_on='meldung_id', right_on='id', how='left')
     mean_sterne = merged_df.groupby('gefahr_id')['sterne'].mean().reset_index()
     mean_sterne.columns = ['id', 'mean_sterne']
     mean_sterne['mean_sterne'] = mean_sterne['mean_sterne'].round(2)
 
-    # Fusionner les résultats avec le DataFrame existant
+    # Merge results with existing DataFrame
     gefahr_counts = pd.merge(gefahr_counts, gefahr[['id', 'bezeichnung_de', 'bezeichnung_fr', 'bezeichnung_it', 'bezeichnung_en']], on='id', how='left')
     gefahr_counts = pd.merge(gefahr_counts, mean_sterne, on='id', how='left')
-    # Merge meldung_ids_list into gefahr_counts
     gefahr_counts = pd.merge(gefahr_counts, meldung_ids_list, on='id', how='left')
 
-
-    # Enregistrer le résultat dans un fichier CSV
+    # Save the result to a CSV file
     if bereichName == 'Betrug / Täuschung':
         bereichName = 'BetrugTauschung'
-
     gefahr_counts.to_csv(f'./figure_data/base/{bereichName}/gefahr_counts_{timeFilter}.csv', index=False)
 
+    # Merge and count treiber_id occurrences
     merged_df_treiber = pd.merge(meldungXgefahr, treiberXmeldung, on='meldung_id')
-    # Map treiber_id to bezeichnung
     treiber_mapping = treiber.set_index('id')['bezeichnung_' + lg].to_dict()
-
-    # Group by gefahr_id and treiber_id, count occurrences, and reshape
     result_df_gefahr = merged_df_treiber.groupby(['gefahr_id', 'treiber_id']).size().unstack(fill_value=0)
     result_df_gefahr = result_df_gefahr.reset_index()
 
@@ -78,31 +84,25 @@ def count_gefahr(timeFilter, bereichName, lg):
     if not result_df_gefahr.empty:
         result_df_gefahr.columns = ['gefahr_id'] + [treiber_mapping.get(col, col) for col in result_df_gefahr.columns[1:]]
     else: 
-        # If result_df_gefahr is empty, create an empty DataFrame with columns from treiber
         result_df_gefahr = pd.DataFrame(columns=['gefahr_id'] + list(treiber_mapping.values()))
 
-
-    # Enregistrer le résultat dans un fichier CSV
+    # Save the result to a CSV file
     result_df_gefahr.to_csv(f'./figure_data/treiber/{bereichName}/gefahr_treiber_counts_{lg}_{timeFilter}.csv', index=False)
 
 
-""" def gefahr_treiber_count(lg):
-    # Charger les fichiers CSV
-    meldungXgefahr = pd.read_csv('./csv-files-filtered/filtered-ad_meldung_ad_gefahr-20231128.csv', sep='#', quotechar='`')
-    treiber = pd.read_csv('./csv-files-filtered/filtered-ad_treiber-20231128.csv', sep='#', quotechar='`')
-    treiberXmeldung = pd.read_csv('./csv-files-filtered/filtered-ad_meldung_ad_treiber-20231128.csv', sep='#', quotechar='`')
-    
-    merged_df = pd.merge(meldungXgefahr, treiberXmeldung, on='meldung_id')
-    result_df_gefahr = merged_df.groupby(['gefahr_id', 'treiber_id']).size().unstack(fill_value=0)
-    result_df_gefahr = result_df_gefahr.reset_index()
-    result_df_gefahr.columns.values[1:] = treiber[f'bezeichnung_{lg}']
-
-    # Enregistrer le résultat dans un fichier CSV
-    result_df_gefahr.to_csv(f'./figure_data/treiber/gefahr_treiber_counts_{lg}.csv', index=False) """
-
-
 def count_matrix(timeFilter, bereichName, lg):
-    # Charger les fichiers CSV
+    """
+    Count occurrences of 'matrix' and related data.
+
+    Parameters:
+    - timeFilter (str): Time filter ('all', 'week', 'month', 'year').
+    - bereichName (str): Bereich name filter ('all' or specific name).
+    - lg (str): Language for treiber bezeichnung ('de', 'fr', 'it', 'en').
+
+    Returns:
+    - CSV files with counts and related data.
+    """
+    # Load CSV files
     meldungXmatrix = pd.read_csv('./csv-files-filtered/filtered-ad_meldung_ad_matrix-20231128.csv', sep='#', quotechar='`')
     matrix = pd.read_csv('./csv-files-filtered/filtered-ad_matrix-20231128.csv', sep='#', quotechar='`')
     meldung = pd.read_csv('./csv-files-filtered/filtered-ad_meldung-20231128.csv', sep='#', quotechar='`')
@@ -111,9 +111,10 @@ def count_matrix(timeFilter, bereichName, lg):
     treiber = pd.read_csv('./csv-files-filtered/filtered-ad_treiber-20231128.csv', sep='#', quotechar='`')
     treiberXmeldung = pd.read_csv('./csv-files-filtered/filtered-ad_meldung_ad_treiber-20231128.csv', sep='#', quotechar='`')
 
+    # Apply time filter
     if timeFilter != 'all':
         meldung['erfDate'] = pd.to_datetime(meldung['erf_date']).dt.date
-        today = pd.to_datetime('2023-11-28') #pd.to_datetime('today').date()
+        today = pd.to_datetime('2023-11-28')
         
         if timeFilter == 'week':
             start = (today - pd.DateOffset(weeks=1)).date()
@@ -129,42 +130,39 @@ def count_matrix(timeFilter, bereichName, lg):
         meldung_ids = meldung['id']
         meldungXmatrix = meldungXmatrix[meldungXmatrix['meldung_id'].isin(meldung_ids)]
 
+    # Apply Bereich filter
     if bereichName != 'all':
         selected_bereich_id = bereich.loc[bereich['bezeichnung_de'] == bereichName, 'id'].values[0]
         meldung_bereich_ids = bereichXmeldung.loc[bereichXmeldung['bereich_id'] == selected_bereich_id, 'meldung_id'].values
         meldungXmatrix = meldungXmatrix[meldungXmatrix['meldung_id'].isin(meldung_bereich_ids)]
 
-    # Group meldung_ids by gefahr_id
+    # Group meldung_ids by matrix_id
     meldung_ids_list = meldungXmatrix.groupby('matrix_id')['meldung_id'].apply(list).reset_index()
     meldung_ids_list.columns = ['id', 'meldung_ids']
 
-    # Compter le nombre de meldung par matrix
+    # Count occurrences of matrix_id
     matrix_counts = meldungXmatrix['matrix_id'].value_counts().reset_index()
     matrix_counts.columns = ['id', 'count']
 
-    # Calculer la moyenne des valeurs de 'sterne' par matrix
+    # Calculate mean 'sterne' values per matrix_id
     merged_df = pd.merge(meldungXmatrix, meldung[['id', 'sterne']], left_on='meldung_id', right_on='id', how='left')
     mean_sterne = merged_df.groupby('matrix_id')['sterne'].mean().reset_index()
     mean_sterne.columns = ['id', 'mean_sterne']
     mean_sterne['mean_sterne'] = mean_sterne['mean_sterne'].round(2)
 
-
-    # Fusionner les résultats avec le DataFrame existant
+    # Merge results with existing DataFrame
     matrix_counts = pd.merge(matrix_counts, matrix[['id', 'bezeichnung_de', 'bezeichnung_fr', 'bezeichnung_it', 'bezeichnung_en']], on='id', how='left')
     matrix_counts = pd.merge(matrix_counts, mean_sterne, on='id', how='left')
     matrix_counts = pd.merge(matrix_counts, meldung_ids_list, on='id', how='left')
-    
-    # Enregistrer le résultat dans un fichier CSV
+
+    # Save the result to a CSV file
     if bereichName == 'Betrug / Täuschung':
         bereichName = 'BetrugTauschung'
-
     matrix_counts.to_csv(f'./figure_data/base/{bereichName}/matrix_counts_{timeFilter}.csv', index=False)
 
-    # Map treiber_id to bezeichnung
-    treiber_mapping = treiber.set_index('id')['bezeichnung_' + lg].to_dict()
-
-    # Merge, Group by gefahr_id and treiber_id, count occurrences, and reshape
+    # Merge and count treiber_id occurrences
     merged_df_treiber = pd.merge(meldungXmatrix, treiberXmeldung, on='meldung_id')
+    treiber_mapping = treiber.set_index('id')['bezeichnung_' + lg].to_dict()
     result_df_matrix = merged_df_treiber.groupby(['matrix_id', 'treiber_id']).size().unstack(fill_value=0)
     result_df_matrix = result_df_matrix.reset_index()
 
@@ -172,28 +170,109 @@ def count_matrix(timeFilter, bereichName, lg):
     if not result_df_matrix.empty:
         result_df_matrix.columns = ['matrix_id'] + [treiber_mapping.get(col, col) for col in result_df_matrix.columns[1:]]
     else: 
-        # If result_df_matrix is empty, create an empty DataFrame with columns from treiber
         result_df_matrix = pd.DataFrame(columns=['matrix_id'] + list(treiber_mapping.values()))
 
-    # Enregistrer le résultat dans un fichier CSV
+    # Save the result to a CSV file
     result_df_matrix.to_csv(f'./figure_data/treiber/{bereichName}/matrix_treiber_counts_{lg}_{timeFilter}.csv', index=False)
 
+def count_steckbrief(timeFilter, bereichName, lg):
+    """
+    Count occurrences of 'steckbrief' and related data.
 
-""" def matrix_treiber_count(lg):
-    # Charger les fichiers CSV
-    meldungXmatrix = pd.read_csv('./csv-files-filtered/filtered-ad_meldung_ad_matrix-20231128.csv', sep='#', quotechar='`')
+    Parameters:
+    - timeFilter (str): Time filter ('all', 'week', 'month', 'year').
+    - bereichName (str): Bereich name filter ('all' or specific name).
+    - lg (str): Language for treiber bezeichnung ('de', 'fr', 'it', 'en').
+
+    Returns:
+    - CSV files with counts and related data.
+    """
+    # Load CSV files
+    meldungXsteckbrief = pd.read_csv('./csv-files-filtered/filtered-ad_meldung_ad_steckbrief-20231128.csv', sep='#', quotechar='`')
+    steckbrief = pd.read_csv('./csv-files-filtered/filtered-ad_steckbrief-20231128.csv', sep='#', quotechar='`')
+    meldung = pd.read_csv('./csv-files-filtered/filtered-ad_meldung-20231128.csv', sep='#', quotechar='`')
+    bereich = pd.read_csv('./csv-files-filtered/filtered-ad_bereich-20231128.csv', sep='#', quotechar='`')
+    bereichXmeldung = pd.read_csv('./csv-files-filtered/filtered-ad_meldung_ad_bereich-20231128.csv', sep='#', quotechar='`')
     treiber = pd.read_csv('./csv-files-filtered/filtered-ad_treiber-20231128.csv', sep='#', quotechar='`')
     treiberXmeldung = pd.read_csv('./csv-files-filtered/filtered-ad_meldung_ad_treiber-20231128.csv', sep='#', quotechar='`')
-    
-    merged_df = pd.merge(meldungXmatrix, treiberXmeldung, on='meldung_id')
-    result_df_matrix = merged_df.groupby(['matrix_id', 'treiber_id']).size().unstack(fill_value=0)
-    result_df_matrix = result_df_matrix.reset_index()
-    result_df_matrix.columns.values[1:] = treiber[f'bezeichnung_{lg}']
+    log = pd.read_csv('./csv-files-filtered/filtered-ad_log-20231128.csv', sep='#', quotechar='`')
 
+    # Apply time filter
+    if timeFilter != 'all':
+        meldung['erfDate'] = pd.to_datetime(meldung['erf_date']).dt.date
+        today = pd.to_datetime('2023-11-28')
 
-    # Enregistrer le résultat dans un fichier CSV
-    result_df_matrix.to_csv(f'./figure_data/treiber/matrix_treiber_counts_{lg}.csv', index=False)
- """
+        if timeFilter == 'week':
+            start = (today - pd.DateOffset(weeks=1)).date()
+        elif timeFilter == 'month':
+            start = (today - pd.DateOffset(months=1)).date()
+        elif timeFilter == 'year':
+            start = (today - pd.DateOffset(years=1)).date()
+
+        meldung = meldung[meldung['erfDate'] >= start]
+        meldung_time_ids = meldung['id']
+        meldungXsteckbrief = meldungXsteckbrief[meldungXsteckbrief['meldung_id'].isin(meldung_time_ids)]
+    else:
+        meldung_ids = meldung['id']
+        meldungXsteckbrief = meldungXsteckbrief[meldungXsteckbrief['meldung_id'].isin(meldung_ids)]
+
+    # Apply Bereich filter
+    if bereichName != 'all':
+        selected_bereich_id = bereich.loc[bereich['bezeichnung_de'] == bereichName, 'id'].values[0]
+        meldung_bereich_ids = bereichXmeldung.loc[bereichXmeldung['bereich_id'] == selected_bereich_id, 'meldung_id'].values
+        meldungXsteckbrief = meldungXsteckbrief[meldungXsteckbrief['meldung_id'].isin(meldung_bereich_ids)]
+
+    # Group meldung_ids by steckbrief_id
+    meldung_ids_list = meldungXsteckbrief.groupby('steckbrief_id')['meldung_id'].apply(list).reset_index()
+    meldung_ids_list.columns = ['id', 'meldung_ids']
+
+    # Count occurrences of steckbrief_id
+    steckbrief_counts = meldungXsteckbrief['steckbrief_id'].value_counts().reset_index()
+    steckbrief_counts.columns = ['id', 'count']
+
+    # Count the number of mutations per steckbrief
+    steckbrief_mut = log['parent_code'].value_counts().reset_index()
+    steckbrief_mut.columns = ['code', 'count_mut']
+
+    # Calculate mean 'sterne' values per steckbrief_id
+    merged_df = pd.merge(meldungXsteckbrief, meldung[['id', 'sterne']], left_on='meldung_id', right_on='id', how='left')
+    mean_sterne = merged_df.groupby('steckbrief_id')['sterne'].mean().reset_index()
+    mean_sterne.columns = ['id', 'mean_sterne']
+    mean_sterne['mean_sterne'] = mean_sterne['mean_sterne'].round(2)
+
+    # Merge results with existing DataFrame
+    steckbrief_counts = pd.merge(steckbrief_counts, steckbrief[['id', 'code', 'titel', 'kurzinfo', 'erf_date', 'mut_date']], on='id', how='left')
+    steckbrief_counts = pd.merge(steckbrief_counts, mean_sterne, on='id', how='left')
+    steckbrief_counts = pd.merge(steckbrief_counts, steckbrief_mut, on='code', how='left')
+    steckbrief_counts = pd.merge(steckbrief_counts, meldung_ids_list, on='id', how='left')
+
+    # Replace empty strings with NaN in the 'titel' column
+    steckbrief_counts['titel'].replace('', pd.NA, inplace=True)
+
+    # Drop rows with NaN in the 'titel' column
+    steckbrief_counts.dropna(subset=['titel'], inplace=True)
+
+    # Save the result to a CSV file
+    if bereichName == 'Betrug / Täuschung':
+        bereichName = 'BetrugTauschung'
+    steckbrief_counts.to_csv(f'./figure_data/base/{bereichName}/steckbrief_counts_{timeFilter}.csv', index=False)
+
+    # Merge and count treiber_id occurrences
+    merged_df_treiber = pd.merge(meldungXsteckbrief, treiberXmeldung, on='meldung_id')
+    treiber_mapping = treiber.set_index('id')['bezeichnung_' + lg].to_dict()
+    result_df_steckbrief = merged_df_treiber.groupby(['steckbrief_id', 'treiber_id']).size().unstack(fill_value=0)
+    result_df_steckbrief = result_df_steckbrief.reset_index()
+
+    # Rename columns using mapped values from treiber
+    if not result_df_steckbrief.empty:
+        result_df_steckbrief.columns = ['steckbrief_id'] + [treiber_mapping.get(col, col) for col in result_df_steckbrief.columns[1:]]
+    else: 
+        # If result_df_steckbrief is empty, create an empty DataFrame with columns from treiber
+        result_df_steckbrief = pd.DataFrame(columns=['steckbrief_id'] + list(treiber_mapping.values()))
+
+    # Save the result to a CSV file
+    result_df_steckbrief.to_csv(f'./figure_data/treiber/{bereichName}/steckbrief_treiber_counts_{lg}_{timeFilter}.csv', index=False)
+
 
 def list_meldung_pro_Gefahr(id):
     # Charger les fichiers CSV
@@ -206,24 +285,33 @@ def list_meldung_pro_Gefahr(id):
     print(meldungs)
 
 
-""" for i in ['de','fr','it','en']:
-    matrix_treiber_count(i)
-    gefahr_treiber_count(i) """
 
+# Read the Bereich CSV file
 bereich_csv = pd.read_csv('./csv-files-filtered/filtered-ad_bereich-20231128.csv', sep='#', quotechar='`')
+
+# Get unique values of Bereich names and add 'all' as an option
 bereich_list = bereich_csv.bezeichnung_de.unique()
 bereich_list = np.append(bereich_list, 'all')
 
+# Loop through each Bereich and language
 for bereich in bereich_list:
     for lg in ['de','fr','it','en']:
+        # Count occurrences of gefahr
         count_gefahr('year', bereich, lg)
         count_gefahr('month', bereich, lg)
         count_gefahr('week', bereich, lg)
         count_gefahr('all', bereich, lg)
 
+        # Count occurrences of matrix
         count_matrix('year', bereich, lg)
         count_matrix('month', bereich, lg)
         count_matrix('week', bereich, lg)
         count_matrix('all', bereich, lg) 
+
+        # Count occurrences of steckbrief
+        count_steckbrief('year', bereich, lg)
+        count_steckbrief('month', bereich, lg)
+        count_steckbrief('week', bereich, lg)
+        count_steckbrief('all', bereich, lg) 
 
 
